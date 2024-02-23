afc9942517e1acc614308bac97ca195777259ca6
emmelance <e.ucunguyab@alustudent.com>
2024-02-24 01:26:30 +0200

    first commit

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,7 +2,6 @@
 """Returns to-do list information for a given employee ID."""
 import requests
 import sys
-
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
